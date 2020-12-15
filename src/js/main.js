b73f9cb4f26ff7ff09647a639f469cd4ee2556ed
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-15 14:40:55 -0700

    master: add color changer

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
-console.log('Working!')
+const headerTag = document.querySelector("[data-target='header']");
+
+const randomNumber = Math.floor(Math.random() * 7) + 1  
+
+headerTag.classList.add(`bg_${randomNumber}`);
