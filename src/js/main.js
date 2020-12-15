f73d87db1f1d71007ef009072cc46c153759c9aa
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-15 15:36:00 -0700

    master: add color profiles

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const headerTag = document.querySelector("[data-selector='header']"); // Grab header element // Note the attribute selector
 const randomNumber = Math.floor(Math.random() * 7) + 1  // Generate a random number between 1 and 7
 
-// headerTag.setAttribute("data-target",`bg_${randomNumber}`); // Onload, add a random number attribute to the background
-headerTag.setAttribute("data-target",`bg_1`); // Onload, add a random number attribute to the background
+headerTag.setAttribute("data-target",`bg_${randomNumber}`); // Onload, add a random number attribute to the background
+
